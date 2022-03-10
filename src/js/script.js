6378cc3df860f73c61cdbed62db48cd77545456d
Jakub Kozik <jakubkozik120@gmail.com>
2022-03-10 19:55:38 +0100

    Fix render function

--- src/js/script.js
+++ src/js/script.js
@@ -6,29 +6,25 @@
       bookProduct: '#template-book', 
     },
     containerOf:{
-      bookList: '#books-list'
+      bookList: '.books-list',
     },
   };
   const templates = {
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.bookProduct).innerHTML),
   };
 
-  const render = function(){
-    // const thisBook = this;
+  const render = function(){    
 
     for(let book of dataSource.books) {      
-       const thisBook = this;
+      const thisBook = this;      
+     
+      const generatedHTML = templates.menuProduct(book);     
 
-      const generatedHTML = templates.menuProduct(dataSource.books);
-
-      thisBook.book = utils.createDOMFromHTML(generatedHTML);
-      
+      thisBook.bookParams = utils.createDOMFromHTML(generatedHTML);
+     
       const bookContainer = document.querySelector(select.containerOf.bookList);
-
-      console.log('bookContainer:', bookContainer);
-
-      bookContainer.appendChild(thisBook.book);
-      // console.log('book:', book);
+      
+      bookContainer.appendChild(thisBook.bookParams);  
     }   
   };
  
